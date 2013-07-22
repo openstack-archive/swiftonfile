a71ab7b9b7c672ab3a41fc6d57532081490f9462
Chetan Risbud <crisbud@redhat.com>
2013-07-22 18:11:52 +0530

    Updating copyrights with current year
    Copyrights are updated across the code to reflect 2013.
    Copyrights message was added to couple of scripts.
    
    Change-Id: I2cfc7bdc6adaa5c9ab0075aab49021a5f0dea7a1
    Signed-off-by: Chetan Risbud <crisbud@redhat.com>
    Reviewed-on: http://review.gluster.org/5371
    Reviewed-by: Luis Pabon <lpabon@redhat.com>
    Tested-by: Luis Pabon <lpabon@redhat.com>

--- tools/functional_tests.sh
+++ tools/functional_tests.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+# Copyright (c) 2013 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 cleanup()
 {
         sudo service memcached stop
         sudo swift-init main stop
         sudo yum -y remove glusterfs-openstack-swift
-	sudo rm -rf /etc/swift > /dev/null 2>&1
+        sudo rm -rf /etc/swift > /dev/null 2>&1
 }
 
 quit()
