f67937757a4cf24829ba805a51ad361b4a122b1a
Chetan Risbud <crisbud@redhat.com>
2014-01-09 11:28:45 +0530

    Release 1.10.0-2
    Change-Id: I5b686fb74124fb9392b647b1198730113f9e05ac
    Signed-off-by: Chetan Risbud <crisbud@redhat.com>
    Reviewed-on: http://review.gluster.org/6670

--- gluster/swift/__init__.py
+++ gluster/swift/__init__.py
@@ -45,6 +45,6 @@ class PkgInfo(object):
 ###
 ### Change the Package version here
 ###
-_pkginfo = PkgInfo('1.10.0', '1', 'gluster_swift', False)
+_pkginfo = PkgInfo('1.10.0', '2', 'gluster_swift', False)
 __version__ = _pkginfo.pretty_version
 __canonical_version__ = _pkginfo.canonical_version
