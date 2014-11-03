3142ed2423c170efc2388d38a9d606c4d1858eaf
Prashanth Pai <ppai@redhat.com>
2014-11-03 17:41:05 +0530

    Add object expiration functests
    This is to be applied only to icehouse branch
    where the old "gluster-swift" code persists.
    
    Change-Id: I2d4698cbb1f89ca7b7bc1a9196106667e6b03e1b
    Signed-off-by: Prashanth Pai <ppai@redhat.com>

---
+++ tools/object_expirer_functional.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Copyright (c) 2013 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This program expects to be run by tox in a virtual python environment
+# so that it does not pollute the host development system
+
+sudo_env()
+{
+    sudo bash -c "PATH=$PATH $*"
+}
+
+cleanup()
+{
+        sudo service memcached stop
+        sudo_env swift-init main stop
+        sudo rm -rf /etc/swift > /dev/null 2>&1
+        sudo rm -rf /mnt/gluster-object/test{,2}/* > /dev/null 2>&1
+        sudo rm -rf /mnt/gluster-object/gsexpiring/* > /dev/null 2>&1
+        sudo setfattr -x user.swift.metadata /mnt/gluster-object/test{,2} > /dev/null 2>&1
+}
+
+quit()
+{
+	echo "$1"
+	exit 1
+}
+
+
+fail()
+{
+	cleanup
+	quit "$1"
+}
+
+### MAIN ###
+
+# Only run if there is no configuration in the system
+if [ -x /etc/swift ] ; then
+	quit "/etc/swift exists, cannot run functional tests."
+fi
+
+# Check the directories exist
+DIRS="/mnt/gluster-object /mnt/gluster-object/test /mnt/gluster-object/test2 /mnt/gluster-object/gsexpiring"
+for d in $DIRS ; do
+	if [ ! -x $d ] ; then
+		quit "$d must exist on an XFS or GlusterFS volume"
+	fi
+done
+
+export SWIFT_TEST_CONFIG_FILE=/etc/swift/test.conf
+
+# Install the configuration files
+sudo mkdir /etc/swift > /dev/null 2>&1
+sudo cp -r test/functional_auth/common_conf/* /etc/swift || fail "Unable to copy configuration files to /etc/swift"
+sudo cp -r test/functional_auth/tempauth/conf/* /etc/swift || fail "Unable to copy configuration files to /etc/swift"
+sudo_env gluster-swift-gen-builders gsexpiring test || fail "Unable to create ring files"
+
+# Remove old files
+sudo rm -rf /mnt/gluster-object/test* > /dev/null 2>&1
+sudo rm -rf /mnt/gluster-object/gsexpiring/* > /dev/null 2>&1
+
+# Start the services
+sudo service memcached start || fail "Unable to start memcached"
+sudo_env swift-init main start || fail "Unable to start swift"
+
+echo "Running functional tests with tempauth"
+
+mkdir functional_tests > /dev/null 2>&1
+nosetests -v --exe \
+	test/object_expirer_functional || fail "Object expirer functional tests failed"
+
+cleanup
+exit 0
